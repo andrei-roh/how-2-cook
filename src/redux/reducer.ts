3c6f1b5529f4420a58e01b03431b07bc6389c274
Andrei Roh <andrei.roh@icloud.com>
2024-04-25 14:09:55 +0200

    H2C-13: Add unit tests for some components

--- src/redux/reducer.ts
+++ src/redux/reducer.ts
@@ -30,43 +30,30 @@ const rootReducer = createReducer(initialState, (builder) => {
   builder
     .addCase(SET_USER_CREATOR, (state, action) => {
       state.user = action.payload;
-      return state;
     })
     .addCase(SET_LOADING_CREATOR, (state, action) => {
       state.loading = action.payload;
-      return state;
     })
     .addCase(SET_ERROR_CREATOR, (state, action) => {
       state.error = action.payload;
-      return state;
     })
     .addCase(ADD_RECIPES_TO_LIST_CREATOR, (state, action) => {
-      return {
-        ...state,
-        recipesList: [...action.payload, ...state.recipesList],
-      };
+      state.recipesList = [...action.payload, ...state.recipesList];
     })
     .addCase(UPDATE_RECIPES_LIST_CREATOR, (state, action) => {
       state.recipesList = action.payload;
-      return state;
     })
     .addCase(ADD_IMAGES_TO_LIST_CREATOR, (state, action) => {
-      return {
-        ...state,
-        imagesList: [...action.payload, ...state.imagesList],
-      };
+      state.imagesList = [...action.payload, ...state.imagesList];
     })
     .addCase(SET_RECIPE_SEARCH_INPUT_CREATOR, (state, action) => {
       state.recipesPage.recipeSearchInput = action.payload;
-      return state;
     })
     .addCase(SET_MANAGE_RECIPES_SCROLL_SIZE_CREATOR, (state, action) => {
       state.recipesPage.scrollSize = action.payload;
-      return state;
     })
     .addCase(SET_MANAGE_RECIPES_SCROLL_DIRECTION_CREATOR, (state, action) => {
       state.recipesPage.scrollDirection = action.payload;
-      return state;
     });
 });
 
